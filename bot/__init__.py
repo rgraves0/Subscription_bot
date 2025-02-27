8373ad72714b3c6e4b8ab81a3cf3147be4d5c5e4
Ryan <91402966+rgraves0@users.noreply.github.com>
2025-02-27 15:28:15 +0700

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,9 @@
+from .logger import LOGGER
+from .config import Config
+
+class CMD:
+    START = "start"
+    AUTH = "auth"
+    ADD_ADMIN = "add_admin"
+    SETTINGS = "settings"
+    DOWNLOAD = "download"
